e5e71421b4524fc99b94c1c0d387b1bbb888701c
AmilcarJaraMerma <amilcarjm84@gmail.com>
2024-03-29 21:48:43 -0500

    submodulos y corrccion de modulo

--- resources/views/theme/adminskote/layout.blade.php
+++ resources/views/theme/adminskote/layout.blade.php
@@ -8,14 +8,14 @@
         <!-- App favicon -->
         <link rel="shortcut icon" href="{{asset("assets/theme/$theme/images/favicon.ico")}}">
 
+        @yield('styles')
+
         <!-- Bootstrap Css -->
         <link href="{{asset("assets/theme/$theme/css/bootstrap.min.css")}}" id="bootstrap-style" rel="stylesheet" type="text/css"/>
         <!-- Icons Css -->
         <link href="{{asset("assets/theme/$theme/css/icons.min.css")}}" rel="stylesheet" type="text/css"/>
         <!-- App Css-->
         <link href="{{asset("assets/theme/$theme/css/app.min.css")}}" id="app-style" rel="stylesheet" type="text/css"/>
-
-        @yield('styles')
 
         <link href="{{asset("css/custom.css")}}"rel="stylesheet" type="text/css"/>
     </head>
@@ -64,12 +64,11 @@
         <script src="{{asset("assets/theme/$theme/libs/node-waves/waves.min.js")}}"></script>
 
         <script src="{{asset("assets/theme/$theme/libs/jquery-ui-dist/jquery-ui.min.js")}}"></script>
-
-        <!-- App js -->
-        <script src="{{asset("assets/theme/$theme/js/app.js")}}"></script>
-
         <script src="{{asset("assets/js/funciones.js")}}"></script>
 
         @yield('scripts')
+
+        <!-- App js -->
+        <script src="{{asset("assets/theme/$theme/js/app.js")}}"></script>
     </body>
 </html>
